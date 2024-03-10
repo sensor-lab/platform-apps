b6cbd2cd59a2254894dff5aab1867607405b138e
Runze <sendrunzedong@gmail.com>
2024-03-10 12:08:03 +0800

    Update code

--- src/js/temperature.js
+++ src/js/temperature.js
@@ -2,9 +2,8 @@ function refreshTime() {
     var date = new Date();
     var time = date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate()
         + ' ' + date.getHours() + ':' + date.getMinutes() + ":" + date.getSeconds();
-    document.getElementById("currentDate").innerHTML = time;
+    document.getElementById("currentDate").innerHTML = `环境监测 ${time}`;
 }
 
-refreshTime();
+refreshTime()
 setInterval(refreshTime, 1000);
-
