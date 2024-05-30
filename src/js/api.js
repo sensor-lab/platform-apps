57aba98378e4773c915fab79a1b1535d12a4e917
Jizhou Zhang <jz1958@nyu.edu>
2024-05-30 14:46:44 -0700

    implemented buzzer player app

--- src/js/api.js
+++ src/js/api.js
@@ -284,3 +284,30 @@ export async function startAdvanceOutput(pin_id, cycle, data) {
         console.log('Error call API:', error);
     }
 }
+
+export async function syncPwmReq(pin_id, operations) {
+    let request = '/hardware/operation';
+    let body = {
+        'event': 'now',
+        'actions': []
+    };
+    for (let i = 0; i < operations.length; i++) {
+        if (operations[i].frequency != 0) {
+            body['actions'].push(["pwm", 0, operations[i].frequency, operations[i].duration, "sync", parseInt(pin_id), 512]);
+        } else {
+            body['actions'].push(["delay", 0, "ms", Math.floor(operations[i].duration * 1000)]);
+        }
+    }
+    try {
+        const response = await fetch(request, {
+            method: 'post',
+            body: JSON.stringify(body)
+        });
+        const data = await response.json();
+        if (data.errorcode != 0) {
+            console.log('API returns error code:', data.errorcode);
+        }
+    } catch (error) {
+        console.log('Error call API:', error);
+    }
+}
