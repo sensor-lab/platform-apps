8cd4a48294dc7e94c8cf6e966122d6f25b25679d
Jizhou Zhang <jz1958@nyu.edu>
2025-05-14 22:01:27 -0700

    implemented onewire temperature sensor/bus

--- src/js/api.js
+++ src/js/api.js
@@ -788,6 +788,43 @@ export function uartHardwareOperation(
   return ret;
 }
 
+export function onewireHardwareOperation(
+  opers,
+  pin,
+  mode,
+  length = 0,
+  data = []
+) {
+  let ret = 0;
+  if (opers.constructor != Array) {
+    ret = -1;
+  } else if (pin == undefined || pin < 0 || pin > 19) {
+    ret = -1;
+  } else if (
+    mode !== "read" &&
+    mode !== "write" &&
+    mode !== "search" &&
+    mode !== "reset"
+  ) {
+    ret = -1;
+  } else if ((mode === "read" || mode === "write") && length <= 0) {
+    ret = -1;
+  } else if (mode === "write" && data.length == 0) {
+    ret = -1;
+  } else {
+    if (mode === "search") {
+      opers.push(["onewire", pin, "search"]);
+    } else if (mode === "reset") {
+      opers.push(["onewire", pin, "reset"]);
+    } else if (mode === "read") {
+      opers.push(["onewire", pin, "read", length]);
+    } else {
+      opers.push(["onewire", pin, "write", length, ...data]);
+    }
+  }
+  return ret;
+}
+
 export function constructNowEvent(opers, cycle = undefined) {
   let now_event;
   if (cycle === undefined) {
