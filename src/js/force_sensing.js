e2915efba1d15cf41f1fa6e82bd3f02527d41668
Jizhou Zhang <jz1958@nyu.edu>
2025-06-11 10:38:56 -0700

    force sensing app improvement

--- src/js/force_sensing.js
+++ src/js/force_sensing.js
@@ -1,45 +1,96 @@
-import {readAdc} from 'sensorsparks.api'
+import {
+  adcHardwareOperation,
+  constructNowEvent,
+  postHardwareOperation,
+} from "sensorsparks.api";
+import Chart from "chart.js/auto";
 
 var state = "config";      //"config", "capture", "display"
-var labels = [];
+var timeLineLabels = [];
 var forceKgVals = [];
 var initialTimeoutSecond = 10;
 var pin_id = -1;
-var forceSenseLineChart = undefined;
+var range = -1;
+
+const forceChart = new Chart(
+  document.getElementById("forceChart"),
+  {
+    type: "line",
+    title: "压力曲线",
+    data: {
+      labels: [],
+      datasets: [
+        {
+          label: '按压力量(kg)',
+          backgroundColor: 'rgba(161, 198, 247, 1)',
+          borderColor: 'rgb(47, 128, 237)',
+          data: [],
+        }
+      ],
+    },
+    options: {
+      responsive: true,
+      animation: false,
+      plugins: {
+        title: {
+          display: true,
+          text: "压力曲线",
+        },
+      },
+      scales: {
+        x: {
+          type: "linear",
+          ticks: {
+            callback: (value) => `${value}秒`,
+          },
+          title: {
+            display: true,
+            text: "时间(s)",
+          },
+        },
+        y: {
+          beginAtZero: false,
+        },
+      },
+    },
+  }
+);
 
 setComponentState("config");
 
 function setComponentState(current_state) {
     state = current_state;
     if (current_state == "capture") {
-        // not showing line chart
-        document.getElementById("lineChart").classList.add("d-none");
-        // not showing start button
-        document.getElementById("startButton").classList.add("d-none");
-        // showing alert
-        document.getElementById("instruction").classList.remove("d-none");
-        // not showing statistics
-        document.getElementById("statistics").classList.add("d-none");
-        // not showing config
-        document.getElementById("configform").classList.add("d-none");
+      // reset chart
+      forceChart.data.labels = [];
+      forceChart.data.datasets[0].data = [];
+      forceChart.update();
+      timeLineLabels = [];
+      forceKgVals = [];
+
+      // not showing start button
+      document.getElementById("startButton").classList.add("d-none");
+      // showing alert
+      document.getElementById("instruction").classList.remove("d-none");
+      // not showing statistics
+      document.getElementById("statistics").classList.add("d-none");
+      // not showing config
+      document.getElementById("configform").classList.add("d-none");
     } else if (current_state == "display") {
-        // showing line chart
-        displayChart()
-        document.getElementById("lineChart").classList.remove("d-none");
-        // showing start button
-        document.getElementById("startButton").classList.remove("d-none");
-        // not showing alert
-        document.getElementById("instruction").classList.add("d-none");
-        // show statistics
-        document.getElementById("statistics").classList.remove("d-none");
-        // not showing config
-        document.getElementById("configform").classList.add("d-none");
+      document.getElementById("forceChart").classList.remove("d-none");
+      // showing start button
+      document.getElementById("startButton").classList.remove("d-none");
+      // not showing alert
+      document.getElementById("instruction").classList.add("d-none");
+      // show statistics
+      document.getElementById("statistics").classList.remove("d-none");
+      // not showing config
+      document.getElementById("configform").classList.add("d-none");
     } else if (current_state == "config") {
-        document.getElementById("lineChart").classList.add("d-none");
-        document.getElementById("instruction").classList.add("d-none");
-        document.getElementById("statistics").classList.add("d-none");
-        document.getElementById("configform").classList.remove("d-none");
-        document.getElementById("startButton").classList.remove("d-none");
+      document.getElementById("instruction").classList.add("d-none");
+      document.getElementById("statistics").classList.add("d-none");
+      document.getElementById("configform").classList.remove("d-none");
+      document.getElementById("startButton").classList.remove("d-none");
     }
 }
 
@@ -55,9 +106,6 @@ function startSensorCapture() {
     function moveBar() {
         var elem = document.getElementById("timeoutCounter");
         if (width == 0) {
-            if (forceSenseLineChart != undefined) {
-                forceSenseLineChart.destroy();
-            }
             calStatistics();
             setComponentState("display");
             clearInterval(progressbar_id);
@@ -84,65 +132,80 @@ function startSensorCapture() {
                 }
             }
         }
-        tableAverage = document.getElementById("statAverage");
-        tableMax = document.getElementById("statMax");
+        const tableAverage = document.getElementById("statAverage");
+        const tableMax = document.getElementById("statMax");
 
         tableAverage.getElementsByTagName("td")[2].innerHTML = average;
-        tableMax.getElementsByTagName("td")[1].innerHTML = labels[maximumIndex];
+        tableMax.getElementsByTagName("td")[1].innerHTML = timeLineLabels[maximumIndex];
         tableMax.getElementsByTagName("td")[2].innerHTML = forceKgVals[maximumIndex];
     }
 
     function calAverage(windowParam) {
         var total = 0;
-        for (i = 0; i < windowParam.length; i++) {
+        for (let i = 0; i < windowParam.length; i++) {
             total += windowParam[i];
         }
         return total/windowParam.length;
     }
+
     function calForce(adcVal) {
         // ADC is 12-bit, MD30-60 datasheet https://xonstorage.blob.core.windows.net/pdf/leanstar_md306010kg_xonjuly20_12_link.pdf
         const fixedDividerResistor = 10; // 10K resistor
         const voltageSupply = 3.3;              // 3.3v
         const voltage = adcVal / (4096) * 3.1;
-        var resistorInKg = (voltageSupply - voltage ) / (voltage / (fixedDividerResistor * 1000)) / 1000
+        var resistorInKohm = (voltageSupply - voltage ) / (voltage / (fixedDividerResistor * 1000)) / 1000
         var forceInKg;
-        if (resistorInKg >= 30) {
+        if (resistorInKohm >= 30) {
             // Range 1: [0kg, 3.5kg] <=> [120kOhm, 30kOhm], slope = -25.71
-            if (resistorInKg >= 120) {
-                resistorInKg = 120;
+            if (resistorInKohm >= 120) {
+                resistorInKohm = 120;
             }
-            forceInKg = (resistorInKg - 120) / (-25.71);
-        } else if (resistorInKg >= 18) {
+            forceInKg = (resistorInKohm - 120) / (-25.71);
+        } else if (resistorInKohm >= 18) {
             // Range 2： [3.5kg, 5kg] <=> [30kOhm, 18kOhm], slope = -8
-            forceInKg = (resistorInKg - 58) / (-8);
-        } else if (resistorInKg >= 10) {
+            forceInKg = (resistorInKohm - 58) / (-8);
+        } else if (resistorInKohm >= 10) {
             // Range 3: [5kg, 10kg] <=> [18kOhm, 10kOhm], slope = -1.6
-            forceInKg = (resistorInKg - 26) / (-1.6);
+            forceInKg = (resistorInKohm - 26) / (-1.6);
         } else {
             // Range 4: [10kg, 20kg] <=> [10kOhm, 5kOhm], slope = -0.5
-            forceInKg = (resistorInKg - 15) / (-0.5);
-            if (forceInKg > 30) {
+            forceInKg = (resistorInKohm - 15) / (-0.5);
+            if (range == 20) {
+              if (forceInKg > 20) {
+                forceInKg = 20;
+              }
+            } else if (range == 30) {
+              if (forceInKg > 30) {
                 forceInKg = 30;
+              }
             }
+            
         }
         return forceInKg;
     }
+
     async function getAdcCapture() {
         if (state != "capture") {
             clearInterval(capture_id);
         } else {
-            singleAdcVal = await readAdc(pin_id);
-
-            if (singleAdcVal != undefined) {
-                labels.push((Date.now() - startTime) / 1000);
+            const opers = [];
+            adcHardwareOperation(opers, pin_id, "3.1v");
+            const now_event = constructNowEvent(opers);
+            const response = await postHardwareOperation(now_event);
+            if (response["errorcode"] === 0) {
+                const singleAdcVal = response["result"][0][0];
                 if (window.length < windowSize) {
                     window.push(singleAdcVal);
                 } else {
                     window.shift();
                     window.push(singleAdcVal);
                 }
-                // calculate average
+                // update chart
+                forceChart.data.labels.push((Date.now() - startTime) / 1000);
+                timeLineLabels.push((Date.now() - startTime) / 1000);
+                forceChart.data.datasets[0].data.push(calForce(calAverage(window)));
                 forceKgVals.push(calForce(calAverage(window)));
+                forceChart.update();
             }
         }
     }
@@ -159,42 +222,13 @@ document.getElementById("pinSelect").addEventListener("change", function(event) 
 });
 
 document.getElementById("startButton").addEventListener("click", function (event) {
-    if (pin_id == -1) {
-        document.getElementById("wrongselection").classList.remove("d-none");
-        setComponentState("config");
-    } else if (state != "capture") {
-        startSensorCapture();
-    }
-})
+  range = document.getElementById("rangeSelect").options[
+    document.getElementById("rangeSelect").selectedIndex].value;
+  if (pin_id == -1 || range == -1) {
+      document.getElementById("wrongselection").classList.remove("d-none");
+      setComponentState("config");
+  } else if (state != "capture") {
+      startSensorCapture();
+  }
+});
 
-function displayChart() {
-    const ctx = document.getElementById("lineChart").getContext('2d');
-    forceSenseLineChart = new Chart(ctx, {
-        type: 'line',
-        data: {
-            labels: labels,
-            datasets: [{
-                label: '按压力量(kg)',
-                backgroundColor: 'rgba(161, 198, 247, 1)',
-                borderColor: 'rgb(47, 128, 237)',
-                data: forceKgVals,
-            }]
-        },
-        options: {
-            scales: {
-              y: {
-                title: {
-                  display: true,
-                  text: '按压力量(kg)'
-                }
-              },
-              x: {
-                title: {
-                    display: true,
-                    text: '时间(s)'
-                }
-              }
-            }     
-          }
-    });
-}
